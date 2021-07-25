41d09cf54c02c76daa9590215a0015e77b041030
ferenc.gulyas <ferenc.gulyas@automizy.com>
2021-07-25 22:21:28 +0200

    frontend jut started

---
+++ secret-server-fe/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="setViewToCreate()" >Create New Secret</button>
+    <button @click="setViewToGet()">Get Secret By Hash</button>
+    <CreateSecret v-if="view === 'create'" />
+    <GetSecret  v-if="view === 'get'" />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import CreateSecret from "./components/CreateSecret.vue";
+import GetSecret from "./components/GetSecret.vue";
+
+export default {
+  name: "App",
+  components: {
+    CreateSecret,
+    GetSecret,
+  },
+  setup() {
+    const view = ref("get");
+
+    const setViewToGet = () => {
+      console.log('pushed')
+      view.value = 'get'
+    };
+
+    const setViewToCreate = () => {
+      view.value = 'create'
+    };
+
+    return {
+      view,
+      setViewToGet,
+      setViewToCreate
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ secret-server-fe/src/components/CreateSecret.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">Create</div>
+</template>
+
+<script>
+export default {
+  name: "CreateSecret",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello {
+  color: gray;
+}
+</style>
